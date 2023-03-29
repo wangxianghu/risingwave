d75a10338ec61ad88834a5e1fed5b5564f1d3011
yufansong <yufansong@foxmail.com>
2023-03-29 17:56:37 -0400

    update environment setting script

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,78 @@
 #Hadoop Related Options
-export HADOOP_HOME=/home/ubuntu/hadoop-3.3.5
-export HADOOP_INSTALL=$HADOOP_HOME
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export YARN_HOME=$HADOOP_HOME
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/nativ"
+wget "https://archive.apache.org/dist/hadoop/core/hadoop-2.7.3/hadoop-2.7.3.tar.gz"
+tar -zxvf hadoop-2.7.3.tar.gz -C ~/
+mv ~/hadoop-2.7.3 ~/hadoop
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo export HADOOP_HOME=/home/ubuntu/hadoop >> ~/.bashrc
+echo export PATH=/home/ubuntu/hadoop/bin:/home/ubuntu/hadoop/sbin:$PATH >> ~/.bashrc
+echo export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 >> ~/.bashrc
+echo export LD_LIBRARY_PATH=${JAVA_HOME}/lib/server:${LD_LIBRARY_PATH} >> ~/.bashrc
+source ~/.bashrc
 
-export LD_LIBRARY_PATH=${JAVA_HOME}/lib/server
+echo export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 >> /home/ubuntu/hadoop/etc/hadoop/yarn-env.sh
+echo export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 >> /home/ubuntu/hadoop/etc/hadoop/hadoop-env.sh
+
+
+
+<< com
+# install java 11
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+java -version; javac -version
+
+
+
+# test ssh localhost
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+ssh localhost
+
+# Forward port
+ssh -L 9870:localhost:9870 ubuntu@54.169.83.184
+
+# core-site.xml:
+
+<configuration>
+    <property>
+        <name>hadoop.tmp.dir</name>
+        <value>file:/home/ubuntu/hadoop/tmp</value>
+        <description>Abase for other temporary directories.</description>
+    </property>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://localhost:9000</value>
+    </property>
+</configuration>
+
+
+# hdfs-site.xml:
+
+<configuration>
+    <property>
+        <name>dfs.replication</name>
+        <value>1</value>
+    </property>
+    <property>
+        <name>dfs.permissions</name>
+        <value>false</value>    
+    </property>
+    <property>
+        <name>dfs.namenode.name.dir</name>
+        <value>file:/home/ubuntu/hadoop/tmp/dfs/name</value>
+    </property>
+    <property>
+        <name>dfs.datanode.data.dir</name>
+        <value>file:/home/ubuntu/hadoop/tmp/dfs/data</value>
+    </property>
+    <property>
+        <name>dfs.namenode.http-address</name>
+        <value>localhost:9870</value>
+    </property>
+    <property>
+        <name>dfs.secondary.http.address</name>
+        <value>localhost:9100</value>
+    </property>
+</configuration>
+
+
